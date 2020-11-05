04fa8fe061f8062ee8470c8c949ff38f321af6c3
Leon Latsch <dev.leonlatsch@gmail.com>
2020-11-05 20:51:39 +0100

    fix vue router redirect issue

--- deploy.bat
+++ deploy.bat
@@ -2,13 +2,17 @@
 
 cd dist
 
-echo [*] Pushing gh-pages branch
-git init
-git add -A
-git commit -m "deploy %DATE%"
+<NUL set /p=[*] Preparing assets... 
+copy index.html 404.html > NUL
+echo Done
 
+<NUL set /p=[*] Pushing gh-pages branch... 
+git init > NUL
+git add -A > NUL
+git commit -m "deploy %DATE%" > NUL
 git push -f git@github.com:leonlatsch/homepage.git master:gh-pages
+echo Done
 
 cd ..
 
-echo [*] Done
+echo [*] Deploy Complete
