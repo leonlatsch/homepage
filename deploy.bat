a6254dbab34387dad8858ee4e0b277ddfe9d9988
Leon Latsch <dev.leonlatsch@gmail.com>
2020-11-05 20:23:31 +0100

    improvement to deploy script

--- deploy.bat
+++ deploy.bat
@@ -2,13 +2,13 @@
 
 cd dist
 
-echo "[*] Pushing gh-pages branch"
+echo [*] Pushing gh-pages branch
 git init
 git add -A
-git commit -m deploy
+git commit -m "deploy %DATE%"
 
 git push -f git@github.com:leonlatsch/homepage.git master:gh-pages
 
 cd ..
 
-echo "[*] Done"
+echo [*] Done
