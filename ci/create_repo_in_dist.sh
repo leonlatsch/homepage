5d7c36ad9e668851d7af1e89981a9ae1dc77ae47
Leon Latsch <50983026+leonlatsch@users.noreply.github.com>
2021-07-16 23:23:58 +0200

    Update create_repo_in_dist.sh

--- ci/create_repo_in_dist.sh
+++ ci/create_repo_in_dist.sh
@@ -4,6 +4,9 @@ cp index.html 404.html
 
 currentDate=`date`
 
+git config user.name "Leon Latsch"
+git config user.email "dev.leonlatsch@gmail.com"
+
 git init
 git add -A
 git commit -m "deploy $currentDate"
