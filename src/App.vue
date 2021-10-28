7dae00d56e481ce45c942b0a6985745ac11390dc
Leon Latsch <50983026+leonlatsch@users.noreply.github.com>
2021-10-28 21:22:36 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
-  background: #0d1117;
+  background: yellow;
   color: white;
   padding-left: 5px;
   padding-right: 5px;
